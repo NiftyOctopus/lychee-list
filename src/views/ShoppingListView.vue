b8b155dadd53dd261442ffe649d6bbd37f2f4bce
Bryan Chambers <bryches@gmail.com>
2021-02-08 10:28:39 -0500

    Added boilerplate to ShoppingListView

---
+++ src/views/ShoppingListView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id='shopping-list-view' class='view'>
+        <div>Shopping List</div>
+    </div>
+</template>
+
+
+
+<script>
+    //import SubComponent from '../components/SubComponent'
+
+    export default {
+        name: 'component-name',
+        components: { /* Subcomponents */ },
+        props: [/* Inputs */],
+        data() { return { /* Local variables */ }},
+        beforeCreate() {},
+        created() {},
+        mounted() {},
+        updated() {},
+        computed: { /*
+            Creates a new property
+            Updates when any dependant property changes */
+        },
+        watch: { /*
+            Watches an existing property
+            Only runs when the watched property changes */
+        },
+        methods: {}
+    }
+</script>
+
+
+
+<style scoped>
+
+</style>
