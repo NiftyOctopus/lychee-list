a6de53dd2baa45cbe2f66ac959b3b344dc956e49
Bryan Chambers <bryches@gmail.com>
2021-03-20 13:28:42 -0400

    Working on Fraction components

---
+++ src/components/Fraction.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class='fraction'>
+        <div class='numerator'  >{{ fraction.n }}</div>
+        <div class='denominator'>{{ fraction.d }}</div>
+        <div>{{ fraction.n / fraction.d }}</div>
+    </div>
+</template>
+
+
+
+<script>
+    //import SubComponent from '../components/SubComponent'
+
+    export default {
+        name: 'fraction',
+        components: { /* Subcomponents */ },
+        props: ['value'],
+        data() { return {
+            denominators: [2, 3, 4, 8, 16]
+        }},
+        beforeCreate() {},
+        created() {},
+        mounted() {},
+        updated() {},
+        computed: {
+            fraction() {
+                let min = 1
+                let d, x, n, e
+                let fraction = {}
+
+                for(let i in this.denominators) {
+                    d = this.denominators[i]
+                    x = this.value * d
+                    n = Math.round(x)
+                    e = x % 1
+
+                    if(e < min) {
+                        min = e
+                        fraction = { n, d }
+                    }
+                }
+
+                return fraction
+            }
+        },
+        watch: {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+
+
+<style scoped>
+    .fraction {
+        margin-top: 100px;
+        display: inline-block;
+        /* border: 1px solid red; */
+    }
+
+    .numerator {
+        border-bottom: 1px solid black;
+    }
+
+    .numerator, .denominator {
+        padding: 2px 5px;
+    }
+</style>
--- src/views/AmountView.vue
+++ src/views/AmountView.vue
@@ -1,6 +1,9 @@
 <template>
     <div class='amount-view view'>
         <view-header title='How much?'></view-header>
+        <div style='margin-top: 150px'><button @click='getRandomValue'>Refresh</button></div>
+        <div>{{ value }}</div>
+        <fraction :value='value'></fraction>
         <view-footer :allowSave=true @save='saveItem'></view-footer>
     </div>
 </template>
@@ -10,12 +13,13 @@
 <script>
     import ViewHeader from '../components/ViewHeader'
     import ViewFooter from '../components/ViewFooter'
+    import Fraction   from '../components/Fraction'
 
     export default {
         name: 'amount-view',
-        components: { ViewHeader, ViewFooter },
+        components: { ViewHeader, ViewFooter, Fraction },
         props: [/* Inputs */],
-        data() { return { /* Local variables */ }},
+        data() { return { value: 1 }},
         beforeCreate() {},
         created() {},
         mounted() {},
@@ -32,6 +36,9 @@
             saveItem() {
                 this.$store.commit('saveItem')
                 this.$router.push('/')
+            },
+            getRandomValue() {
+                this.value = Math.random()
             }
         }
     }
