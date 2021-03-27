cf349ccaf47e13b49b290dcfefb49471c41737d0
Bryan Chambers <bryches@gmail.com>
2021-03-27 11:05:45 -0400

    Search component styling

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,9 @@
 <template>
     <div class='search'>
-        <input type='text' v-model='text'>
+        <div class='wrapper'>
+            <input type='text' v-model='text'>
+            <img class='icon' src='../assets/icons/search.svg'>
+        </div>
     </div>
 </template>
 
@@ -35,7 +38,30 @@
 
 
 <style scoped>
-    input {
+
+    .wrapper {
+        position: relative;
+        display: inline-block;
+    }
+
+    input[type=text] {
+        appearance: none;
+        /* -webkit-appearance: none; */
         font-size: 16px;
+        font-weight: bold;
+        text-align: center;
+        border: 2px solid lightgray;
+        outline: none;
+    }
+
+    input:focus {
+        border: 2px solid #e8385b;
+    }
+
+    .icon {
+        position: absolute;
+        top: 6px;
+        right: 6px;
+        opacity: 0.4;
     }
 </style>
