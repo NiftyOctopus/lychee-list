c00b988fba4cdaaa995c1ae84eb4c423aee24587
Bryan Chambers <bryches@gmail.com>
2021-08-15 11:04:54 -0400

    Refactored Item component

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -5,11 +5,15 @@
             <img class='icon delete' src='../assets/icons/trash.svg' @click='deleteItem'>
         </div>
 
-        <div class='wrapper' v-bind:class="{ open: open }" @touchstart='startSwipe' @touchmove='swiping' @touchend='endSwipe'>
+        <div class='wrapper'
+            v-bind:class='{ open }'
+            @touchstart='startSwipe'
+            @touchmove='swiping'>
+        
             <div class='name' @click='toggleItem'>
-                <span v-bind:class="{ done: item.done }">
+                <span v-bind:class='{ done: item.done }'>
                     <span>{{ item.name }}</span>
-                    <span v-if='amount'>, {{ amount }}</span>
+                    <span v-if='amountWithUnit'>, {{ amountWithUnit }}</span>
                 </span>
             </div>
         </div>
@@ -25,24 +29,16 @@
     export default {
         name: 'item',
         mixins: [fractionize],
-        components: { /* Subcomponents */ },
         props: ['i', 'item'],
         data() { return { open: false, pos: 0, start: 0 }},
-        beforeCreate() {},
-        created() {},
-        mounted() {},
-        updated() {},
         computed: {
             ...mapState(['showCompleted']),
-            value() {
-                return this.item.amount
-            },
-            amount() {
+
+            amountWithUnit() {
                 if(!this.item.amount || !this.item.unit) { return null }
-
                 const whole = Math.floor(this.item.amount)
-                let amt = whole > 0 ? whole : ''
 
+                let amt = whole > 0 ? whole : ''
                 const n = this.fraction.n
                 const d = this.fraction.d
                 if(n > 0) { amt = amt + ' ' + n + '/' + d }
@@ -50,51 +46,46 @@
                 return amt + ' ' + this.item.unit
             }
         },
-        watch: { /*
-            Watches an existing property
-            Only runs when the watched property changes */
-        },
         methods: {
             async toggleItem() {
                 if(this.item.recipe === 0) {
-                    const done = this.item.done ? 0 : 1
-                    //this.$store.commit('log', 'Toggle item ' + this.item.name + '[' + this.item.id + ']')
-                    this.$store.commit('toggleItem', this.item)
-                    const n = await this.$db.items.update(this.item.id, { done })
-                    //this.$store.commit('log', 'Updated ' + n + ' records in db')
+                    try {
+                        const done = this.item.done ? 0 : 1
+                        this.$store.commit('toggleItem', this.item)
+                        await this.$db.items.update(this.item.id, { done })
+                    
+                    } catch(e) { alert(e) }
                 }
             },
             startSwipe(event) {
                 this.start = event.touches[0].clientX
             },
             swiping(event) {
-                //this.end = event.touches[0].clientX
                 const delta = this.start - event.touches[0].clientX
                 
                 if(!this.open && delta >  50) { this.open = true  }
                 if( this.open && delta < -50) { this.open = false }
             },
-            endSwipe(event) {
-
-            },
             editItem() {
-                const item = Object.assign({}, this.item)
-                item.prev  = this.item.category
-                item.i     = this.i
+                try {
+                    const item = Object.assign({ i: this.i, prev: this.item.category }, this.item)
+                    this.$store.commit('setItem', item)
 
-                this.$store.commit('setItem', item)
-                this.$router.push(this.item.unit ? '/amount' : '/unit')
+                    let route = this.item.unit ? '/amount' : '/unit'
+                    if(this.item.category == 'Other') { route = '/category' }
+                    this.$router.push(route)
+                
+                } catch(e) { alert(e) }
             },
             async deleteItem() {
                 try {
-                    this.open = false
+                    this.open  = false
                     const item = Object.assign({ i: this.i }, this.item)
+
                     this.$store.commit('deleteItem', item)
                     await this.$db.items.delete(this.item.id)
                 
-                } catch(e) {
-                    alert(e)
-                }
+                } catch(e) { alert(e) }
             }
         }
     }
@@ -105,72 +96,47 @@
 <style scoped>
     .item {
         position: relative;
-        /* border: 1px solid red; */
-        margin: 2px 0;
-        /* font-size: 70px; */
+        margin:   -1px 0;
+        background-color: lightgray;
+        
+        border-top:    1px solid lightgray;
+        border-bottom: 1px solid lightgray;
     }
 
     .wrapper {
-        display: flex;
         position: relative;
-        
-        padding: 2px;
-        right: 0;
-        /* border: 1px solid green; */
-        background-color: white;
-        transition: all 250ms;
-        transition-timing-function: cubic-bezier(0.32, 1.69, 0.6, 0.8);
-        z-index: 20;
-    }
+        right:    0;
+        padding:  7px;
+        z-index:  10;
 
-    .name {
-        /* display:  flex; */
-        position: relative;
-        /* border: 1px solid black; */
-        margin: 0 auto;
+        background-color: white;
+
+        transition: right 250ms;
+        transition-timing-function: cubic-bezier(0.32, 1.69, 0.6, 0.8);
     }
 
     .done {
         text-decoration: line-through;
         color: gray;
-        /* position: absolute;
-        top:    0;
-        left:  -3px;
-        right: -3px;
-        height: 50%;
-        border-bottom: 1px solid black; */
     }
 
     .actions {
-        display: flex;
+        display:         flex;
+        flex-flow:       row nowrap;
+        justify-content: center;
+        align-items:     center;
+
         position: absolute;
-        right: 5px;
-        height: 100%;
-        /* background-color: gray; */
-        z-index: -1;
-        /* border: 1px solid blue; */
-        padding: 0;
-        z-index: 10;
+        right:    5px;
+        height:   100%;
     }
 
     .open {
-        right: 74px;
+        right: 80px;
         background-color: #f2f2f2;
     }
 
-
     .icon {
-        /* border: 1px solid black; */
-        /* background-color: lightblue; */
-        padding: 0 4px;
-        margin: 0 1px;
-    }
-
-    .edit {
-        background-color: lightskyblue
-    }
-
-    .delete {
-        background-color: lightsalmon;
+        margin: 0 5px;
     }
 </style>
