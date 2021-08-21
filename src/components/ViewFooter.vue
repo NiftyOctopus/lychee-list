e6e6662c11a55b7d809856259241df19ccaf02d5
Bryan Chambers <bryches@gmail.com>
2021-08-21 19:51:11 -0400

    Restyled and refactored ViewFooter

--- src/components/ViewFooter.vue
+++ src/components/ViewFooter.vue
@@ -1,42 +1,23 @@
 <template>
     <div class='view-footer'>
-        <img v-if='allowAdd'  class='icon' src='../assets/icons/plus-circle.svg' @click='add'>
-        <img v-if='allowSave' class='icon' src='../assets/icons/save.svg' @click='save'>
-        <div v-if='allowDelete' id='delete' @click='del'>DELETE</div>
-
-        <div id='log'>
-            <div v-for='(msg, index) in log' :key='index'>
-                <span>{{ msg }}</span>
-            </div>
-        </div>
+        <img v-if='allowAdd'    class='icon' src='../assets/icons/plus-circle.svg' @click='add'>
+        <img v-if='allowSave'   class='icon' src='../assets/icons/save.svg'        @click='save'>
+        <img v-if='allowDelete' class='icon' src='../assets/icons/trash.svg'       @click='del'>
     </div>
 </template>
 
 
 
 <script>
-    //import SubComponent from '../components/SubComponent'
     import { convert }  from '../mixins/convert'
     import { mapState } from 'vuex'
 
     export default {
         name: 'view-footer',
         mixins: [convert],
-        components: { /* Subcomponents */ },
         props: ['allowAdd', 'allowSave', 'allowDelete'],
-        data() { return { /* Local variables */ }},
-        beforeCreate() {},
-        created() {},
-        mounted() {},
-        updated() {},
-        computed: { /*
-            Creates a new property
-            Updates when any dependant property changes */
+        computed: {
             ...mapState(['item', 'log']),
-        },
-        watch: { /*
-            Watches an existing property
-            Only runs when the watched property changes */
         },
         methods: {
             add() {
@@ -62,22 +43,15 @@
         bottom:     0;
         left:       0;
         right:      0;
-        padding:    5px 5px 25px 5px;
-        text-align: center;
-        background-color: rgba(255, 255, 255, 0.7);
+        padding:    10px;
+        display:            flex;
+        flex-flow:          row nowrap;
+        justify-content:    center;
+        align-items:        center;
+        background-color: lightgray;
     }
 
-    #delete {
-        color: salmon;
-        font-weight: bold;
-        font-size: 14px;
-        margin-top: 10px;
-    }
-
-    #log {
-        padding-left: 5px;
-        font-family: monospace;
-        text-align: left;
-        font-size: 12px;
+    img {
+        margin: 0 10px;
     }
 </style>
