798bb3d0bed6f24304737fcc09acfc183dcad91d
Bryan Chambers <bryches@gmail.com>
2021-08-24 12:57:13 -0400

    Fine tuned fraction logic

--- src/views/AmountView.vue
+++ src/views/AmountView.vue
@@ -3,9 +3,17 @@
         <view-header title='How much?'  @back='back'></view-header>
         
         <div id='amount'>
-            <fraction :value='item.amount'></fraction>
-            <div>{{ item.unit }}</div>
+            <div id='whole'>{{ whole }}</div>
+        
+            <div id='fraction' v-if='fraction.n > 0 && fraction.n < fraction.d'>
+                <div id='n'>{{ fraction.n }}</div>
+                <div id='d'>{{ fraction.d }}</div>
+            </div>
+
+            <div id='unit'>{{ item.unit }}</div>
         </div>
+
+        <div id='value'>{{ value.toFixed(6) }}</div>
         
         <div id='adjusters'>
             <adjuster d=1></adjuster>
@@ -24,15 +32,16 @@
 <script>
     import ViewHeader from '../components/ViewHeader'
     import ViewFooter from '../components/ViewFooter'
-    import Fraction   from '../components/Fraction'
     import Adjuster   from '../components/Adjuster'
+    import { fractionize } from '../mixins/fractionize'
 
     import { mapState } from 'vuex'
 
 
     export default {
         name: 'amount-view',
-        components: { ViewHeader, ViewFooter, Fraction, Adjuster },
+        components: { ViewHeader, ViewFooter, Adjuster },
+        mixins: [fractionize],
         props: [/* Inputs */],
         data() { return {} },
         beforeCreate() {},
@@ -42,7 +51,8 @@
         computed: { /*
             Creates a new property
             Updates when any dependant property changes */
-            ...mapState(['item'])
+            ...mapState(['item']),
+            value() { return this.item.amount }
         },
         watch: { /*
             Watches an existing property
@@ -59,6 +69,43 @@
 
 
 <style scoped>
+    #amount {
+        /* border: 1px solid gray; */
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #whole {
+        font-size: 30px;
+        margin-right: 2px;
+        padding: 15px 0;
+    }
+
+    #fraction {
+        /* border: 1px solid red; */
+    }
+
+    #fraction > #n {
+        border-bottom: 1px solid black;
+    }
+
+    #fraction > #n, #fraction > #d {
+        margin:  0px 5px;
+        padding: 2px 8px;
+    }
+
+    #unit {
+        /* padding-bottom: 1px; */
+        margin-left: 2px;
+    }
+
+    #value {
+        font-size: 14px;
+        color: gray;
+    }
+
     #adjusters {
         display: flex;
         flex-flow: row nowrap;
