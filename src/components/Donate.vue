46daee30ed4da7b4ec0a54c790162180e4340ec8
Bryan Chambers <bgchambers@pm.me>
2021-09-16 11:29:36 -0400

    Added Donate popup

---
+++ src/components/Donate.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id='donate' @click='clickToCancel'>
+        <div id='modal'>
+            <h4>Enjoying Lychee List?</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+
+            <a href='https://www.buymeacoffee.com/niftyoctopus'><button>Donate</button></a>
+            <button @click='cancel'>Maybe Later</button>
+            <a href='https://niftyoctopus.com'>Other Apps</a>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+    export default {
+        name: 'donate',
+        components: { /* Subcomponents */ },
+        props: ['buttons'],
+        data() { return { /* Local variables */ }},
+        beforeCreate() {},
+        created() {},
+        mounted() {},
+        updated() {},
+        computed: { /*
+            Creates a new property
+            Updates when any dependant property changes */
+        },
+        watch: { /*
+            Watches an existing property
+            Only runs when the watched property changes */
+        },
+        methods: {
+            clickToCancel(event) {
+                if(event.target.id == 'donate') {
+                    this.$emit('cancel')
+                }
+            },
+            cancel() {
+                this.$emit('cancel')
+            }
+        }
+    }
+</script>
+
+
+
+<style scoped>
+    #donate {
+        z-index: 1000;
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        
+        display:         flex;
+        justify-content: center;
+        align-items:     center;
+        overflow:        hidden;
+
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    #modal {
+        max-width: 80%;
+        padding: 5px;
+        background-color: white;
+        border: 1px solid black;
+        display: flex;
+        flex-flow:       column nowrap;
+        justify-content: center;
+        align-items:     stretch;
+    }
+
+    h4 {
+        border: 1px solid blue;
+        margin: 0;
+    }
+
+    p {
+        text-align: left;
+    }
+
+    button {
+        border: 1px solid red;
+        padding: 5px 10px;
+        margin: 5px;
+    }
+</style>
--- src/components/ViewFooter.vue
+++ src/components/ViewFooter.vue
@@ -1,23 +1,37 @@
 <template>
     <div class='view-footer'>
-        <img v-if='allowAdd'    class='icon' src='../assets/icons/plus-circle.svg' @click='add'>
-        <img v-if='allowSave'   class='icon' src='../assets/icons/save.svg'        @click='save'>
-        <img v-if='allowDelete' class='icon' src='../assets/icons/trash.svg'       @click='del'>
+        <div id='footer-left'>
+            <img class='icon' src='../assets/icons/heart.svg' @click='like'>
+        </div>
+
+        <div id='footer-center'>
+            <img v-if='allowAdd'  class='icon' src='../assets/icons/plus-circle.svg' @click='add'>
+            <img v-if='allowSave' class='icon' src='../assets/icons/save.svg'        @click='save'>
+        </div>
+
+        <div id='footer-right'>
+            <img v-if='allowDelete' class='icon' src='../assets/icons/trash.svg' @click='del'>
+        </div>
+
+        <donate v-if='donate' @cancel='close'></donate>
     </div>
 </template>
 
 
 
 <script>
-    import { convert }  from '../mixins/convert'
+    import   Donate     from '../components/Donate'
+    import { convert  } from '../mixins/convert'
     import { mapState } from 'vuex'
 
     export default {
         name: 'view-footer',
+        components: { Donate },
         mixins: [convert],
         props: ['allowAdd', 'allowSave', 'allowDelete'],
+        data() { return { donate: false }},
         computed: {
-            ...mapState(['item', 'log']),
+            ...mapState(['item']),
         },
         methods: {
             add() {
@@ -30,6 +44,12 @@
                 await this.saveItem()
                 this.$router.push(this.item.recipe ? 'recipe/' + this.item.recipe : '/')
                 this.$store.commit('clearItem')
+            },
+            like() {
+                this.donate = true
+            },
+            close() {
+                this.donate = false
             }
         }
     }
@@ -43,16 +63,28 @@
         bottom:     0;
         left:       0;
         right:      0;
-        padding:    10px;
+        padding:    5px;
         z-index:    100;
         display:            flex;
-        flex-flow:          row nowrap;
-        justify-content:    center;
-        align-items:        center;
+        flex-direction:     row;
+        justify-content:    space-between;
         background-color: #f2f2f2;
     }
 
-    img {
-        margin: 0 10px;
+    .view-footer > div {
+        flex-grow:  1;
+        flex-basis: 0;
+    }
+
+    #footer-left {
+        text-align: left;
+    }
+
+    #footer-right {
+        text-align: right;
+    }
+
+    .icon {
+        margin: 0 3px;
     }
 </style>
