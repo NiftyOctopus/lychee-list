810274589269a9af805035f616a2f2f9eea406a6
Bryan Chambers <bgchambers@pm.me>
2021-12-01 18:32:48 -0500

    Removed Vue 3 migration build settings Updated package lock file

--- vue.config.js
+++ vue.config.js
@@ -11,18 +11,5 @@ module.exports = {
                 }
             })
         ]
-    },
-    /*
-    chainWebpack: config => {
-        config.resolve.alias.set('vue', '@vue/compat')
-    
-        config.module.rule('vue').use('vue-loader').tap(options => {
-            return {
-                ...options,
-                compilerOptions: {
-                    compatConfig: { MODE: 3 }
-                }
-            }
-        })
-    }*/
+    }
 }
