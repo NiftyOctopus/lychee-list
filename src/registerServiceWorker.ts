eed37866f868041d3ee5d78172b079724ee2fb69
Bryan Chambers <bgchambers@pm.me>
2022-01-31 21:27:44 -0500

    Added messages to service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,32 +1,43 @@
-/* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
+import { store } from './store'
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+if(process.env.NODE_ENV === 'production') {
+    register(`${process.env.BASE_URL}service-worker.js`, {
+        
+        ready () {
+            const text = 'Loaded from cache'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        registered() {
+            const text = 'Service worker registered'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        cached() {
+            const text = 'Cached for offline use'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        updatefound() {
+            const text = 'Downloading updates'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        updated() {
+            const text = 'Updates available'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        offline () {
+            const text = 'Offline mode'
+            console.log(text)
+            store.dispatch('message', { text })
+        },
+        error (error) {
+            const text = 'Service worker error'
+            console.error('text', error)
+            store.dispatch('message', { text })
+        }
+    })
 }
