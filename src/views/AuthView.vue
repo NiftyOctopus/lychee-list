27b0cbda35079fd4f309269738668f899dba6647
Bryan Chambers <bgchambers@pm.me>
2022-02-23 15:25:54 -0500

    Working on auth logic

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -9,8 +9,8 @@
         <div><input v-model='email' type='text' placeholder='email'></div>
         <div><button @click='requestCode()'>Get Code</button></div>
 
-        <div><input type='text' placeholder='code'></div>
-        <div><button>{{ type }}</button></div>
+        <div><input v-model='code.email' type='text' placeholder='code'></div>
+        <div><button @click='auth()'>{{ type }}</button></div>
 
         <!-- <view-footer></view-footer> -->
     </div>
@@ -29,7 +29,8 @@
         props: [/* Inputs */],
         data() { return {
             type: 'auth',
-            email: null
+            email: null,
+            code: { email: null, client: null }
         }},
         beforeCreate() {},
         created() {
@@ -49,13 +50,29 @@
         },
         methods: {
             requestCode() {
-                const endpoint = 'https://lychee-api.niftyoctopus.workers.dev/'
-                //const endpoint = 'http://127.0.0.1:8787/'
+                //const endpoint = 'https://lychee-api.niftyoctopus.workers.dev/'
+                const endpoint = 'http://127.0.0.1:8787/'
 
                 this.$store.dispatch('message', { text: 'Requesting code' })
                 
                 this.$http.post(endpoint + 'code', { email: this.email }).then((res) => {
+                    this.code.client = res.data.code
                     this.$store.dispatch('message', { text: 'Client Code: ' + res.data.code })
+                })
+            },
+            auth() {
+                //const endpoint = 'https://lychee-api.niftyoctopus.workers.dev/'
+                const endpoint = 'http://127.0.0.1:8787/'
+
+                this.$store.dispatch('message', { text: 'Authenticating' })
+                
+                const creds = { email: this.email, code: this.code }
+                this.$http.post(endpoint + this.type, creds).then((res) => {
+                    console.log(res.data)
+                    this.$store.dispatch('message', { text: res.data.auth })
+                }).catch((e) => {
+                    console.log(e.toString())
+                    this.$store.dispatch('message', { text: e.toString() })
                 })
             }
         }
