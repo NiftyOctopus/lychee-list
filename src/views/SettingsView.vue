226e097705a41e48731c3180b46a568123c74df5
Bryan Chambers <bgchambers@pm.me>
2022-03-05 16:16:28 -0500

    Add updated property to all records

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -8,8 +8,9 @@
 
         <div><router-link to='/auth/signup'><button>Signup</button></router-link></div>
         <div><router-link to='/auth/login'><button>Login</button></router-link></div>
-        <div><button @click='checkIDs()'>Check IDs</button></div>
-        <div><button @click='killOrphans()'>Kill orphans</button></div>
+        <div><button @click='countRecords()'>Count records</button></div>
+        <div><button @click='countUpdated()'>Count updated</button></div>
+        <div><button @click='setUpdated()'>Set updated</button></div>
 
         <!-- <view-footer></view-footer> -->
     </div>
@@ -43,44 +44,29 @@
             Only runs when the watched property changes */
         },
         methods: {
-            async checkIDs() {
-                const recipes = await this.$db.recipes.toCollection().primaryKeys()
-                const items   = await this.$db.items.toCollection().primaryKeys()
+            async countRecords() {
+                const recipes = await this.$db.recipes.count()
+                const items   = await this.$db.items.count()
+                const total   = recipes + items
                 
-                const ids  = recipes.concat(items)
-                const ints = ids.filter(id => parseInt(id) == id)
-
-                const text = ints.length + ' ints of ' + ids.length + ' total'
-                this.$store.dispatch('message', { text })
+                this.$store.dispatch('message', { text: recipes + ' Recipes' })
+                this.$store.dispatch('message', { text: items   + ' Items' })
+                this.$store.dispatch('message', { text: total   + ' Records' })
             },
-            async killOrphans() {
-                const recipes = await this.getRecipeIDs()
-                const items   = await this.$db.items.toArray()
-
-                let id, list
-                let orphans = []
-
-                for(let item of items) {
-                    id   = item.recipe
-                    list = !id || id === 0 || id === '0' || id === 'none'
-
-                    if(!list && !recipes[id + ''] && !recipes[parseInt(id)]) {
-                        orphans.push(item.id)
-                    }
-                }
-
-                if(orphans.length <= 10) { await this.$db.items.bulkDelete(orphans) }
-                this.$store.dispatch('message', { text: 'Killed ' + orphans.length + ' orphans' })
+            async countUpdated() {
+                const recipes = await this.$db.recipes.where('updated').notEqual('*').count()
+                const items   = await this.$db.items.where('updated').notEqual('*').count()
+                const total   = recipes + items
+                
+                this.$store.dispatch('message', { text: total })
             },
-            async getRecipeIDs() {
-                let ids = {}
-                const recipes = await this.$db.recipes.toCollection().primaryKeys()
+            async setUpdated() {
+                const updates = { updated: new Date().toISOString() }
+                const recipes = await this.$db.recipes.toCollection().modify(updates)
+                const items   = await this.$db.items.toCollection().modify(updates)
+                const total   = recipes + items
 
-                for(let id of recipes) {
-                    ids[id] = true
-                }
-
-                return ids
+                this.$store.dispatch('message', { text: 'Modified ' + total + ' records' })
             }
         }
     }
