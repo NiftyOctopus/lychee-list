ed353810760ea6ace3bd688001e1f67a27f0137e
Bryan Chambers <bgchambers@pm.me>
2022-04-01 15:28:00 -0400

    Call ping route from service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,6 +8,15 @@ self.addEventListener('activate', event => {
     event.waitUntil(clients.claim())
 })
 
+const ENDPOINTS = {
+    dev:  'http://127.0.0.1:8787/api/',
+    prod: 'https://lychee.niftyoctopus.com/api/'
+}
+
+const ENV = self.location.hostname === 'localhost' ? 'dev' : 'prod'
+const API = ENDPOINTS[ENV]
+
 self.addEventListener('message', event => {
     console.log('Hello from service worker')
+    self.setTimeout(() => fetch(API + 'ping'), 1000)
 })
